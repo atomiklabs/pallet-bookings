3c32b23ee414068dbb00863330d163298fa80588
Tomasz Kopacki <tk-o@users.noreply.github.com>
2021-05-21 21:09:40 +1000

    feat(bookings): initial ocw
    This commit introduces crypto features to the pallet to allow the OCW to send singed transactions. Does not compile yet.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub use pallet::*;
+pub use pallet::*; // reexport in crate namespace for `construct_runtime!`
 
 #[cfg(test)]
 mod mock;
@@ -13,63 +13,121 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
-	use frame_system::pallet_prelude::*;
+    use frame_support::{
+        dispatch::DispatchResultWithPostInfo,
+        storage::{types::ValueQuery, StorageValue},
+        traits::{Hooks, IsType},
+    };
+    use frame_system::{
+        offchain::{
+            AppCrypto,
+            CreateSignedTransaction,
+            SendSignedTransaction,
+            // SignedPayload, Signer, SigningTypes, SubmitTransaction,
+        },
+        pallet_prelude::*,
+    };
+    use sp_core::crypto::KeyTypeId;
 
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-	}
+    /// Defines application identifier for crypto keys of this module.
+    ///
+    /// Every module that deals with signatures needs to declare its unique identifier for
+    /// its crypto keys.
+    /// When an offchain worker is signing transactions it's going to request keys from type
+    /// `KeyTypeId` via the keystore to sign the transaction.
+    /// The keys can be inserted manually via RPC (see `author_insertKey`).
+    pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"demo");
 
-	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
-	pub struct Pallet<T>(_);
+    /// Based on the above `KeyTypeId` we need to generate a pallet-specific crypto type wrapper.
+    /// We can utilize the supported crypto kinds (`sr25519`, `ed25519` and `ecdsa`) and augment
+    /// them with the pallet-specific identifier.
+    pub mod crypto {
+        use super::KEY_TYPE;
+        use sp_core::sr25519::Signature as Sr25519Signature;
+        use sp_runtime::app_crypto::{app_crypto, sr25519};
+        use sp_runtime::{traits::Verify, MultiSignature, MultiSigner};
 
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	pub type Something<T> = StorageValue<_, u32>;
+        app_crypto!(sr25519, KEY_TYPE);
 
-	#[pallet::event]
-	#[pallet::metadata(T::AccountId = "AccountId")]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		SomethingStored(u32, T::AccountId),
-	}
+        pub struct TestAuthId;
+        // implemented for ocw-runtime
+        impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
+            type RuntimeAppPublic = Public;
+            type GenericSignature = sp_core::sr25519::Signature;
+            type GenericPublic = sp_core::sr25519::Public;
+        }
 
-	#[pallet::error]
-	pub enum Error<T> {
-		NoneValue,
-		StorageOverflow,
-	}
+        // implemented for mock runtime in test
+        impl
+            frame_system::offchain::AppCrypto<
+                <Sr25519Signature as Verify>::Signer,
+                Sr25519Signature,
+            > for TestAuthId
+        {
+            type RuntimeAppPublic = Public;
+            type GenericSignature = sp_core::sr25519::Signature;
+            type GenericPublic = sp_core::sr25519::Public;
+        }
+    }
 
-	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+    #[pallet::config]
+    pub trait Config: frame_system::Config + CreateSignedTransaction<Call<Self>> {
+        /// The identifier type for an offchain worker.
+        type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
+        /// The overarching dispatch call type.
+        type Call: From<Call<Self>>;
+        /// The overarching event type.
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+    }
 
-	#[pallet::call]
-	impl<T:Config> Pallet<T> {
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
 
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResultWithPostInfo {
-			let who = ensure_signed(origin)?;
+    #[pallet::storage]
+    #[pallet::getter(fn something)]
+    pub(super) type Something<T: Config> = StorageValue<_, u32, ValueQuery>;
 
-			<Something<T>>::put(something);
+    #[pallet::event]
+    #[pallet::metadata(T::AccountId = "AccountId")]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        SomethingStored(u32, T::AccountId),
+    }
 
-			Self::deposit_event(Event::SomethingStored(something, who));
-			Ok(().into())
-		}
+    #[pallet::error]
+    pub enum Error<T> {
+        NoneValue,
+        StorageOverflow,
+    }
 
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
-			let _who = ensure_signed(origin)?;
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
-			match <Something<T>>::get() {
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					<Something<T>>::put(new);
-					Ok(().into())
-				},
-			}
-		}
-	}
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+        pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResultWithPostInfo {
+            let who = ensure_signed(origin)?;
+
+            <Something<T>>::put(something);
+
+            Self::deposit_event(Event::SomethingStored(something, who));
+            Ok(().into())
+        }
+
+        #[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
+        pub fn cause_error(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
+            let _who = ensure_signed(origin)?;
+
+            match <Something<T>>::get() {
+                None => Err(Error::<T>::NoneValue)?,
+                Some(old) => {
+                    let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+                    <Something<T>>::put(new);
+                    Ok(().into())
+                }
+            }
+        }
+    }
 }
