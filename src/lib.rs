270c0d0e98d79169fe43b025616ffc96d781a600
korzewski <korzewski@gmail.com>
2021-05-26 10:03:01 +0200

    Fixed pallet imports

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,62 @@
 
 pub use pallet::*; // reexport in crate namespace for `construct_runtime!`
 
+use frame_support::{
+    dispatch::DispatchResultWithPostInfo,
+    pallet_prelude::*,
+};
+use frame_system::{
+    offchain::{
+        AppCrypto,
+        CreateSignedTransaction,
+        SendSignedTransaction,
+        // SignedPayload, Signer, SigningTypes, SubmitTransaction,
+    },
+    pallet_prelude::*,
+};
+use sp_core::crypto::KeyTypeId;
+
+/// Defines application identifier for crypto keys of this module.
+///
+/// Every module that deals with signatures needs to declare its unique identifier for
+/// its crypto keys.
+/// When an offchain worker is signing transactions it's going to request keys from type
+/// `KeyTypeId` via the keystore to sign the transaction.
+/// The keys can be inserted manually via RPC (see `author_insertKey`).
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"demo");
+
+/// Based on the above `KeyTypeId` we need to generate a pallet-specific crypto type wrapper.
+/// We can utilize the supported crypto kinds (`sr25519`, `ed25519` and `ecdsa`) and augment
+/// them with the pallet-specific identifier.
+pub mod crypto {
+    use super::KEY_TYPE;
+    use sp_core::sr25519::Signature as Sr25519Signature;
+    use sp_runtime::app_crypto::{app_crypto, sr25519};
+    use sp_runtime::{traits::Verify, MultiSignature, MultiSigner};
+
+    app_crypto!(sr25519, KEY_TYPE);
+
+    pub struct TestAuthId;
+    // implemented for ocw-runtime
+    impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
+        type RuntimeAppPublic = Public;
+        type GenericSignature = sp_core::sr25519::Signature;
+        type GenericPublic = sp_core::sr25519::Public;
+    }
+
+    // implemented for mock runtime in test
+    impl
+        frame_system::offchain::AppCrypto<
+            <Sr25519Signature as Verify>::Signer,
+            Sr25519Signature,
+        > for TestAuthId
+    {
+        type RuntimeAppPublic = Public;
+        type GenericSignature = sp_core::sr25519::Signature;
+        type GenericPublic = sp_core::sr25519::Public;
+    }
+}
+
 #[cfg(test)]
 mod mock;
 
@@ -13,62 +69,7 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::{
-        dispatch::DispatchResultWithPostInfo,
-        storage::{types::ValueQuery, StorageValue},
-        traits::{Hooks, IsType},
-    };
-    use frame_system::{
-        offchain::{
-            AppCrypto,
-            CreateSignedTransaction,
-            SendSignedTransaction,
-            // SignedPayload, Signer, SigningTypes, SubmitTransaction,
-        },
-        pallet_prelude::*,
-    };
-    use sp_core::crypto::KeyTypeId;
-
-    /// Defines application identifier for crypto keys of this module.
-    ///
-    /// Every module that deals with signatures needs to declare its unique identifier for
-    /// its crypto keys.
-    /// When an offchain worker is signing transactions it's going to request keys from type
-    /// `KeyTypeId` via the keystore to sign the transaction.
-    /// The keys can be inserted manually via RPC (see `author_insertKey`).
-    pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"demo");
-
-    /// Based on the above `KeyTypeId` we need to generate a pallet-specific crypto type wrapper.
-    /// We can utilize the supported crypto kinds (`sr25519`, `ed25519` and `ecdsa`) and augment
-    /// them with the pallet-specific identifier.
-    pub mod crypto {
-        use super::KEY_TYPE;
-        use sp_core::sr25519::Signature as Sr25519Signature;
-        use sp_runtime::app_crypto::{app_crypto, sr25519};
-        use sp_runtime::{traits::Verify, MultiSignature, MultiSigner};
-
-        app_crypto!(sr25519, KEY_TYPE);
-
-        pub struct TestAuthId;
-        // implemented for ocw-runtime
-        impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
-            type RuntimeAppPublic = Public;
-            type GenericSignature = sp_core::sr25519::Signature;
-            type GenericPublic = sp_core::sr25519::Public;
-        }
-
-        // implemented for mock runtime in test
-        impl
-            frame_system::offchain::AppCrypto<
-                <Sr25519Signature as Verify>::Signer,
-                Sr25519Signature,
-            > for TestAuthId
-        {
-            type RuntimeAppPublic = Public;
-            type GenericSignature = sp_core::sr25519::Signature;
-            type GenericPublic = sp_core::sr25519::Public;
-        }
-    }
+    use super::*;
 
     #[pallet::config]
     pub trait Config: frame_system::Config + CreateSignedTransaction<Call<Self>> {
@@ -86,7 +87,7 @@ pub mod pallet {
 
     #[pallet::storage]
     #[pallet::getter(fn something)]
-    pub(super) type Something<T: Config> = StorageValue<_, u32, ValueQuery>;
+    pub(super) type Something<T: Config> = StorageValue<_, u32>;
 
     #[pallet::event]
     #[pallet::metadata(T::AccountId = "AccountId")]
